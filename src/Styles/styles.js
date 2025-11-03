32bdf93ef1da94e61645fccfc01eaa2276552cf8
Lucas Pulgaci <lucaspac032@hotmail.com>
2025-11-03 11:53:41 -0300

    refactor: move component styles to 'styles.js' for better organization and readability
    This centralizes style variables and improves code clarity by separating structure from design.

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,6 +1,6 @@
 const styleForChangeBodyTheme = ` text-black bg-blue-300 rounded-lg w-1/2 m-2 font-[Orbitron] tracking-wide absolute bottom-0 animate-glow`
 const stylesForInitialCardLink = `flex bg-blue-500 w-1/2 text-[--font-color-secundary] rounded-lg self-center p-4 hover:bg-blue-500`
-const stylesForInitialCardDiv = `flex bg-blue-900 rounded-xl w-1/2 m-auto max-w-[90%]`
+const stylesForInitialCardDiv = `flex bg-blue-900 rounded-xl w-1/2 m-auto max-w-[90%] animate-glow`
 const styleForRoadmapCardsLink = ` flex bg-blue-500 w-1/2 text-[--font-color-secundary] rounded-lg self-center p-4 hover:bg-blue-900`
 const styleForCard = `flex flex-col border border-[--font-color-primary]  h-auto rounded-md p-4`
 
