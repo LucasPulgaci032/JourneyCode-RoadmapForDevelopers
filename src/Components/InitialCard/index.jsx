b69956ebee690cf82d2050a3fc2db408db84c1ee
Lucas Pulgaci <lucaspac032@hotmail.com>
2025-11-18 20:34:39 -0300

    feat: add conditional rendering for object array with content and code blocks
    Each object contains:
    - index 0: a string or an array of strings, rendered in different components depending on the type
    - index 1: a formatted code string rendered using ReactMarkdown
    
    The array is mapped with conditional logic to choose the appropriate component for each index.

--- src/Components/InitialCard/index.jsx
+++ src/Components/InitialCard/index.jsx
@@ -1,19 +1,65 @@
 import { LangsContainer } from "../../RoadmapComponents/Container";
-import { LangDescription } from "../../RoadmapComponents/exports";
+import { LangDescription, TopicsTitle } from "../../RoadmapComponents/exports";
 import { LangTitle } from "../../RoadmapComponents/LangTitle";
-import { inicialDescription, logicAndAlgoritms } from "./text";
+import { dataTypes, dataTypesCode, inicialDescription, logicAndAlgoritms, operators, operatorsCode, varAndConstCode, varAndConsts } from "./text";
+import ReactMarkdown from 'react-markdown'
+import {  stylesForDivCode1, stylesForDivCode2 } from "../../Styles/styles";
+import { useContext } from "react";
+import { ThemeProvider } from "../../Provider/ContextProvider";
+
+const initialContent = [
+  {
+    Variáveis: [varAndConsts,varAndConstCode]
+  },
+  {
+   Tipagem: [dataTypes,dataTypesCode]
+  },
+  {
+   Operadores: [operators,operatorsCode]
+  }
+]
+
+
 
 export function InitialCardPage(){
+  const {color} = useContext(ThemeProvider)
     return(
         <LangsContainer>
             <LangTitle>Lógica de programação : conceitos iniciais</LangTitle>
             <LangDescription>
                 {inicialDescription}
             </LangDescription>
-          <h2>Algoritmos e Logica</h2>
+          <TopicsTitle>
+            Algoritmos e Lógica
+          </TopicsTitle>
           <p>
             {logicAndAlgoritms}
           </p>
+          {initialContent.map((item,idx)=>(
+            <div key={idx} className="flex flex-col gap-8 max-w-[90%]
+            min-w-[90%] ">
+                <TopicsTitle>
+                    {Object.keys(item)}
+                </TopicsTitle>
+                {Array.isArray(Object.values(item)[0][0]) ?
+                Object.values(item)[0][0]
+                .map((item,idx)=>(
+                 
+                  <ReactMarkdown key={idx}>
+                    {item}
+                  </ReactMarkdown>
+                )) :
+                  <LangDescription>
+                    {Object.values(item)[0][0]}
+                  </LangDescription>
+                }
+              
+              <section className={color ? stylesForDivCode1 : stylesForDivCode2}>
+              <ReactMarkdown>{Object.values(item)[0][1]}</ReactMarkdown> 
+              </section>    
+            </div>
+          ))}
         </LangsContainer>
     )
 }
+
