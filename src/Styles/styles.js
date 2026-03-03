51ed35704f119fe2522269fc8870d2602da86d1b
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-03-02 23:02:30 -0300

    feat: create new text styles

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,20 +1,22 @@
-const styleForChangeBodyTheme = ` text-black bg-blue-300 rounded-lg w-1/2 m-2 font-[Orbitron] tracking-wide absolute bottom-0 animate-glow`
-const stylesForInitialCardLink = `flex bg-blue-500 w-1/2 text-[--font-color-secundary] rounded-lg self-center p-4 hover:bg-blue-500`
-const stylesForInitialCardDiv = `flex bg-blue-900 rounded-xl w-1/2 m-auto max-w-[90%] animate-glow`
-const styleForRoadmapCardsLink = ` flex bg-blue-500 w-1/2 text-[--font-color-secundary] rounded-lg self-center p-4 hover:bg-blue-900`
-const styleForCard = `flex flex-col border border-[--font-color-primary]  h-auto rounded-md p-4`
+const stylesForMenuLinks = `text-blue-300 font-bold  w-1/2 bg-opacity-0 p-2 hover:scale-110 self-start transition-transform duration-200 `;
+const stylesForInitialCardLink = `flex bg-blue-500 w-1/2 text-[--font-color-secundary] rounded-lg self-center p-4 hover:bg-blue-500 hover:text-[110%]`;
+const stylesForInitialCardDiv = `flex bg-blue-900 rounded-xl w-1/2 m-auto max-w-[50%] animate-glow text-blue-300`;
+const stylesForDivCode1 = `flex flex-col  bg-[--bg-color-section1] p-3 self-center text-[--font-color-section1] rounded-md gap-4 w-full max-w-[80%] ] border-solid border-white border-4`;
+const stylesForDivCode2 = `flex flex-col  bg-[--bg-color-section2] p-3 self-center text-[--font-color-section2] rounded-md gap-4 w-full max-w-[80%] border-solid border-[--font-color-section2] border-4 `;
+const styleForRoadmapCardsLink = ` flex bg-blue-500 w-1/2 text-[--font-color-secundary]  rounded-lg self-center p-3 hover:bg-blue-900 `;
+const styleForCard = `flex flex-col border border-[--font-color-primary] h-auto rounded-md p-5 gap-1`;
 
+const stylesForLangsContainer = `flex flex-col p-6 m-auto max-w-[90%] gap-10 justify-center items-center `;
+const stylesForLangTitle = `text-4xl  text-[--font-color-primary] text-shadow-md`;
 
-
-const stylesForLangsContainer = `flex flex-col p-4 m-auto max-w-[90%] gap-10 justify-center items-center `
-const stylesForLangTitle = `text-4xl  text-[--font-color-primary] text-shadow-md`
 export {
-    styleForChangeBodyTheme,
-    stylesForInitialCardLink,
-    stylesForInitialCardDiv,
-    styleForRoadmapCardsLink,
-    styleForCard,
-    stylesForLangsContainer,
-    stylesForLangTitle
-    
-}
+  stylesForMenuLinks,
+  stylesForInitialCardLink,
+  stylesForInitialCardDiv,
+  stylesForDivCode1,
+  stylesForDivCode2,
+  styleForRoadmapCardsLink,
+  styleForCard,
+  stylesForLangsContainer,
+  stylesForLangTitle,
+};
