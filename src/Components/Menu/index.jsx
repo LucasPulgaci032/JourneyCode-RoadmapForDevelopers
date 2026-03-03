900730d42dcf00d41662c764c594392fcc83bd91
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-03-02 22:42:06 -0300

    feat(auth): add async function handleLogout to remove token from localStorage and redirect user to login page

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,45 +1,46 @@
-import { CiMenuBurger } from "react-icons/ci"
-import {Link} from "react-router-dom"
-import ChangeBodyTheme from "../body"
-
-
+import { CiMenuBurger } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import ChangeBodyTheme from "../body/ChangeBodyThemeButton";
+import { stylesForMenuLinks } from "../../Styles/styles";
+import { useLocation , useNavigate} from "react-router-dom";
 
 const urls = [
-    {id:1,
-     name:"Inicio",
-     path:"/"
-    },
-    {id:2,
-     name:"Sobre",
-     path:"/about"
-    }
-]
-
-
-
-export function BtnMenu({onClick,...props}){
-
+  { id: 1, name: "Inicio", path: "/roadmaps" },
+  { id: 2, name: "Sobre", path: "/about" },
   
+];
 
-    return( 
-        <button 
-        {...props} 
-        onClick={onClick} 
-        className={`flex justify-end text-4xl p-4  bottom-0 right-0 fixed ${onClick ? "text-[--font-color-primary]" : "text-[--font-color-secundary]"}`}>
-                <CiMenuBurger/>
-        </button>
-    )
+
+export function BtnMenu({ onClick, ...props }) {
+  const locationButton = useLocation();
+
+
+  return (
+    <button
+      onClick={onClick}
+      className={`flex justify-end  text-4xl p-4 right-2 mr-8 fixed  hover:text-5xl ${onClick ? "text-[--font-color-primary]" : "text-[--font-color-secundary]"} ${locationButton.pathname == "/" ? "bottom-5" : "bottom-0"}`}
+    >
+      <CiMenuBurger />
+    </button>
+  );
 }
 
-export function Menu({...props}){
+export function Menu({ ...props }) {
+    const navigate = useNavigate()
 
-
-    return (
-        <nav {...props}>
-            {urls.map((item,index)=>(
-                <Link key={index}to={item.path}>{item.name}</Link>
-            ))}
-             <ChangeBodyTheme>Alterar cor</ChangeBodyTheme>
-        </nav>
-    )
+const handleLogout = () =>{
+  localStorage.removeItem("token");
+  navigate('/')
+}
+  return (
+    <nav {...props}>
+      <ChangeBodyTheme>Alterar tema</ChangeBodyTheme>
+      {urls.map((item, index) => (
+        <Link className={stylesForMenuLinks} key={index} to={item.path}>
+          {item.name}
+        </Link>
+      ))}
+      <button onClick={handleLogout} className={stylesForMenuLinks}>Sair</button>
+    </nav>
+  );
 }
